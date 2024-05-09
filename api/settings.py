2531f12da9b8f65a780f2b73add44acbfc3af00e
yamburnerexpress <kevinnorwood92@gmail.com>
2024-05-08 20:39:16 -0500

    env variables

--- api/settings.py
+++ api/settings.py
@@ -1,14 +1,14 @@
 import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+# from os.path import join, dirname
+# from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
 
-DB_URL = os.getenv("DB_URL")
+DB_URL = os.environ["DB_URL"]
 ACCESS_TOKEN_EXPIRE_MINUTES = 60  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 ALGORITHM = "HS256"
-JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')   # should be kept secret
-JWT_REFRESH_SECRET_KEY = os.getenv('JWT_REFRESH_SECRET_KEY')   # should be kept secret
+JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']   # should be kept secret
+JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']   # should be kept secret
 
