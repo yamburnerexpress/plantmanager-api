1b2ca4e814207d5c1ae41c33870cd37f72019883
yamburnerexpress <kevinnorwood92@gmail.com>
2024-05-09 07:01:28 -0500

    middleware init

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from api.routers import auth, users, plants, user_plants, user_groups
@@ -7,6 +7,22 @@ from api.routers import auth, users, plants, user_plants, user_groups
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
