df4748a304c75d781c8cc0f88b537b8622857357
yamburnerexpress <kevinnorwood92@gmail.com>
2024-05-20 13:09:16 -0500

    async user plants

--- api/routers/user_plants.py
+++ api/routers/user_plants.py
@@ -12,31 +12,31 @@ router = APIRouter(
 )
 
 @router.post("/create/")
-def create_plant(current_user: Annotated[schemas.User, Depends(get_current_user)], user_plant: schemas.UserPlantBase, db: Session = Depends(get_db)):
+async def create_plant(current_user: Annotated[schemas.User, Depends(get_current_user)], user_plant: schemas.UserPlantBase, db: Session = Depends(get_db)):
     res = crud.create_user_plant(db=db, user_plant=user_plant, current_user=current_user)
     return {"id": res.id}
 
 @router.post("/{plant_id}/update/", response_model=schemas.UserPlantInfoResponse)
-def update_user_plant(current_user: Annotated[schemas.User, Depends(get_current_user)], plant_id: int, user_plant: schemas.UserPlantUpdate, db: Session = Depends(get_db)):
+async def update_user_plant(current_user: Annotated[schemas.User, Depends(get_current_user)], plant_id: int, user_plant: schemas.UserPlantUpdate, db: Session = Depends(get_db)):
     res = crud.update_user_plant(db=db, plant_id=plant_id, user_plant=user_plant, current_user=current_user)
     return res
 
 @router.get("/", response_model=list[schemas.UserDashboardGroup])  # schemas.UserPlantInfoResponse
-def get_user_plants(current_user: Annotated[schemas.User, Depends(get_current_user)], db: Session = Depends(get_db)):
+async def get_user_plants(current_user: Annotated[schemas.User, Depends(get_current_user)], db: Session = Depends(get_db)):
     res = crud.get_user_groups(db=db, current_user=current_user)
     return res
 
 @router.get("/{plant_id}/", response_model=schemas.UserPlantInfoResponse)
-def get_user_plant_by_id(plant_id: int, current_user: Annotated[schemas.User, Depends(get_current_user)], db: Session = Depends(get_db)):
+async def get_user_plant_by_id(plant_id: int, current_user: Annotated[schemas.User, Depends(get_current_user)], db: Session = Depends(get_db)):
     return crud.get_user_plant_by_id(db=db, plant_id=plant_id, current_user=current_user)
 
 @router.post("/water/")
-def water_plants(plant_ids: schemas.WaterPlantsInput, current_user: Annotated[schemas.User, Depends(get_current_user)], db: Session = Depends(get_db)):
+async def water_plants(plant_ids: schemas.WaterPlantsInput, current_user: Annotated[schemas.User, Depends(get_current_user)], db: Session = Depends(get_db)):
     plants_watered = crud.water_plants(db=db, plant_ids=plant_ids, current_user=current_user)
     return {"plants_watered": plants_watered}
 
 @router.post("/{plant_id}/notes/")
-def post_user_plant_note(
+async def post_user_plant_note(
         plant_id: int,
         note: schemas.UserPlantNoteBase,
         current_user: Annotated[schemas.User, Depends(get_current_user)],
@@ -48,7 +48,7 @@ def post_user_plant_note(
     return JSONResponse(status_code=200, content={"message": "Note created successfully"})
 
 @router.get("/{plant_id}/notes/", response_model=list[schemas.UserPlantNoteResponse])
-def get_user_plant_notes(
+async def get_user_plant_notes(
         plant_id: int,
         current_user: Annotated[int, Depends(get_current_user)],
         db: Annotated[Session, Depends(get_db)]
@@ -57,7 +57,7 @@ def get_user_plant_notes(
     return q
 
 @router.delete("/{plant_id}/delete/")
-def delete_user_plant(
+async def delete_user_plant(
         plant_id: int,
         current_user: Annotated[int, Depends(get_current_user)],
         db: Annotated[Session, Depends(get_db)]
